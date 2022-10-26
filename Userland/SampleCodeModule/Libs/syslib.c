c39fa5d6996e1af15da64c858e6107371d54f4aa
Juan Ramiro Castro <juancastro@itba.edu.ar>
2022-10-26 13:05:17 -0300

    Fix for linkedit problems

--- Userland/SampleCodeModule/Libs/syslib.c
+++ Userland/SampleCodeModule/Libs/syslib.c
@@ -1,6 +1,6 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-#include "include/syslib.h"
+#include "../include/syslib.h"
 
 unsigned int strlen(const char *str){
     unsigned int len = 0;
--- Userland/SampleCodeModule/Tests/test_util.c
+++ Userland/SampleCodeModule/Tests/test_util.c
@@ -1,6 +1,6 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-#include "include/test_util.h"
+#include "../include/test_util.h"
 
 //Random
 static uint32_t m_z = 362436069;
