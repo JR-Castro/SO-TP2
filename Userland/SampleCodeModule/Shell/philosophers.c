d980b11dd5eb6438de74e1ca4d4664fc129b9cee
Juan Ramiro Castro <juancastro@itba.edu.ar>
2022-10-31 16:56:03 -0300

    Still not working

--- Userland/SampleCodeModule/Shell/philosophers.c
+++ Userland/SampleCodeModule/Shell/philosophers.c
@@ -2,14 +2,13 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "../include/philosophers.h"
 
-#define BLOCK 10
-#define STARTPHILS 2
+#define ARRAYSIZE 100
+#define STARTPHILS 5
 
-static int philoStatus[100];
-static uint64_t philoPid[100];
-static char *semaphores[100];
+static int philoStatus[ARRAYSIZE];
+static uint64_t philoPid[ARRAYSIZE];
+static char *semaphores[ARRAYSIZE];
 static int lock = 0;
-static int size = 0;
 static int philos = 0;
 
 
@@ -23,22 +22,25 @@ void printphilostatus() {
     putchar('\n');
 }
 
-// argv = { name, philoId }
+// argv = { name, id, leftSem }
 _Noreturn static int philosopher(int argc, char **argv) {
     int id = satoi(argv[1]);
-    int left = id;
-    int right = (id + 1) % philos;
+    char *left = argv[1];
 
-    sys_sem_open(semaphores[right], 0, 1);
-    sys_sem_open(semaphores[left], 0, 1);
+    acquire(&lock);
+    char *right = semaphores[(id+1) % philos];
+    release(&lock);
+
+    sys_sem_open(left, 0, 1);
+    sys_sem_open(right, 0, 1);
 
     while (1) {
         if (id % 2 == 0) {
-            sys_sem_wait(semaphores[right]);
-            sys_sem_wait(semaphores[left]);
+            sys_sem_wait(right);
+            sys_sem_wait(left);
         } else {
-            sys_sem_wait(semaphores[left]);
-            sys_sem_wait(semaphores[right]);
+            sys_sem_wait(left);
+            sys_sem_wait(right);
         }
 
         acquire(&lock);
@@ -53,57 +55,46 @@ _Noreturn static int philosopher(int argc, char **argv) {
         printphilostatus();
         release(&lock);
 
-        sys_sem_post(semaphores[left]);
-        sys_sem_post(semaphores[right]);
-    }
-}
+        sys_sem_post(left);
+        sys_sem_post(right);
 
-void growArrays() {
-//    char **newSemaphores = sys_alloc(sizeof(char *) * (size + BLOCK));
-//    uint64_t *newPids = sys_alloc(sizeof(uint64_t) * (size + BLOCK));
-//    int *newStatus = sys_alloc(sizeof(int *) * (size + BLOCK));
-//    size += BLOCK;
-//    for (int i = 0; i < philos; ++i) {
-//        newSemaphores[i] = semaphores[i];
-//        newPids[i] = philoPid[i];
-//        newStatus[i] = philoStatus[i];
-//    }
-//    sys_free(semaphores);
-//    sys_free(philoPid);
-//    sys_free(philoStatus);
-//    semaphores = newSemaphores;
-//    philoPid = newPids;
-//    philoStatus = newStatus;
+        acquire(&lock);
+        if (strcmp(right, semaphores[(id+1)%philos]) != 0) {
+            sys_sem_close(right);
+            right = semaphores[(id+1)%philos];
+            sys_sem_open(right, 0, 1);
+        }
+        release(&lock);
+    }
 }
 
 void addphilo() {
-    char *p_argv[2] = {"philo"};
+    char *p_argv[3];
     char buffer[30];
-    char semname[30];
-    strcpy(semname, "philo");
-//    if (philos == size - 1)
-//        growArrays();
-    if (philos == 100)
+    char left[30];
+
+    if (philos >= ARRAYSIZE)
         return;
+
+    strcpy(left, "phylo");
+
     itoa(philos, buffer);
-    strcat(semname, buffer);
-    char *aux = sys_alloc(sizeof(char) * (strlen(semname) + 1));
-    strcpy(aux, semname);
-    semaphores[philos] = aux;
+    strcat(left, buffer);
+
+    semaphores[philos] = sys_alloc(sizeof(char) * (strlen(left) + 1));
+    strcpy(semaphores[philos], left);
+
+    p_argv[0] = left;
     p_argv[1] = buffer;
-    sys_sem_open(semaphores[philos], 0, 1);
+    p_argv[2] = left;
+
     philoStatus[philos] = 0;
     philos = philos + 1;
-    char philoString[30];
-    itoa(philos, philoString);
-    puts(philoString);
-    if ((philoPid[philos - 1] = sys_createProcess((void (*)(int, char **)) philosopher, 2, p_argv)) == 0) {
+
+    if ((philoPid[philos - 1] = sys_createProcess((void (*)(int, char **)) philosopher, 3, p_argv)) == 0) {
         fputs("Philosopher creation failed\n", STDERR);
-        sys_free(semaphores[philos - 1]);
         philos = philos - 1;
     }
-    sys_block(philoPid[philos - 1]);
-    sys_free(aux);
 }
 
 void removephilo() {
@@ -111,27 +102,17 @@ void removephilo() {
         return;
     }
     philos--;
-    sys_kill(philoPid[philos]);
-    sys_sem_close(semaphores[philos]);
     sys_free(semaphores[philos]);
+    sys_kill(philoPid[philos]);
 }
 
 static void safeexit();
 
 void philosophers(int argc, char **argv) {
     acquire(&lock);
-    size = BLOCK;
-//    philoStatus = sys_alloc(sizeof(int) * BLOCK);
-//    semaphores = sys_alloc(sizeof(char *) * BLOCK);
-//    philoPid = sys_alloc(sizeof(uint64_t) * BLOCK);
-
 
     for (int i = 0; i < STARTPHILS; i++) {
         addphilo();
-    }
-
-    for (int i = 0; i < STARTPHILS; ++i) {
-        sys_unblock(philoPid[i]);
     }
     release(&lock);
 
@@ -141,7 +122,6 @@ void philosophers(int argc, char **argv) {
         if (c == 'a') {
             acquire(&lock);
             addphilo();
-            sys_unblock(philoPid[philos - 1]);
             release(&lock);
             continue;
         }
@@ -166,15 +146,6 @@ void philosophers(int argc, char **argv) {
 static void safeexit() {
     for (int i = 0; i < philos; ++i) {
         sys_kill(philoPid[i]);
+        sys_free(semaphores[i]);
     }
-//    if (philoStatus) {
-//        sys_free(philoStatus);
-//    }
-//    if (semaphores) {
-//        for (int i = 0; i < philos; ++i) {
-//            sys_sem_close(semaphores[i]);
-//            sys_free(semaphores[i]);
-//        }
-//        sys_free(semaphores);
-//    }
 }
