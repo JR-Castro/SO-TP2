f9f442e7f755edc1b4009484bfd9aef453024600
Juan Ramiro Castro <juancastro@itba.edu.ar>
2022-11-03 22:58:08 -0300

    pvs.sh accepts memory manager indicator

--- pvs.sh
+++ pvs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+make clean -C /root/Toolchain > /dev/null
+make clean -C /root/ > /dev/null
 make -C /root/Toolchain > /dev/null
-pvs-studio-analyzer trace -- make gdb -C /root/ > /dev/null
+pvs-studio-analyzer trace -- make MM=$1 gdb -C /root/ > /dev/null
 pvs-studio-analyzer analyze > /dev/null 2> /dev/null
 plog-converter -a '64:1,2,3;GA:1,2,3;OP:1,2,3' -t tasklist -o report.tasks PVS-Studio.log  > /dev/null
 rm -f strace_out
